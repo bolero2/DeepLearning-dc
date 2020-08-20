9246fba1b34791593e87ec0c6e83613cab02f580
ipsuck <sheocjf1025@gmail.com>
2020-08-20 21:52:47 +0900

    Update config.py

--- yolo-v1-tensorflow/config.py
+++ yolo-v1-tensorflow/config.py
@@ -2,7 +2,7 @@ image_Width = 448
 image_Height = 448
 channel = 3
 
-label_size = 20     # pascal VOC 2012 Dataset
+label_size = 20     # => len(label_full), pascal VOC 2012 Dataset
 
 grid = 7
 batchsize = 4
@@ -11,6 +11,17 @@ Learning_Rate = 0.00001
 box_per_cell = 2        # one cell have 2 box
 boundary1 = grid * grid * label_size  # 7 * 7 * 20
 boundary2 = boundary1 + grid * grid * box_per_cell  # 7 * 7 * 20 + 7 * 7 *2
+
+"""
+About Training variables 
+"""
+Filenames_Eval = []
+Filenames_Train = []
+
+index_train = 0
+index_eval = 0
+
+ForEpoch = 40
 
 TrainDir = "C:\\dataset\\VOC2012\\JPEGImages\\"  # 300,000 images
 TrainDir_Annot = "C:\\dataset\\VOC2012\\Annotations\\"
@@ -22,24 +33,19 @@ ModelName = "4lab_detection1"
 ModelDir = "C:\\1+works\\2+Python\\1+Saver\\" + ModelName + "\\"
 # ModelDir = "D:\\0+2020ML\\1+Saver\\" + ModelName + "\\"
 # ModelDir = "/content/drive/My Drive/" + ModelName + "/"
+
+Total_Train = 17125
+
+"""
+if you want to load pretrained-weights, then you should input ckpt file's path
+"""
 weight_file = None
 # weight_file = "C:\\1+works\\2+Python\\1+Saver\\4lab_detection1\\4lab_detection1_Epoch_1.ckpt"
-
-Filenames_Eval = []
-Filenames_Train = []
-
-index_train = 0
-index_eval = 0
-
-ForEpoch = 40
 
 label_full = ['person', 'bird', 'cat', 'cow', 'dog',
               'horse', 'sheep', 'aeroplane', 'bicycle', 'boat',
               'bus', 'car', 'motorbike', 'train', 'bottle',
               'chair', 'diningtable', 'pottedplant', 'sofa', 'tvmonitor']
-
-label_size = 20     # => len(label_full)
-Total_Train = 17125
 
 relu_alpha = 0.5
 
