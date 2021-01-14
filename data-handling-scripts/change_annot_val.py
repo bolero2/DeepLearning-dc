26074a451bc98a46f9bfaf1cd2c0b660e24bbde8
Ubuntu <yb@YBGPU3.s11hy2ldy3ke1mfyi5uwvu2clb.ex.internal.cloudapp.net>
2021-01-14 07:40:22 +0000

    modified change_annot_val.py

--- data-handling-scripts/change_annot_val.py
+++ data-handling-scripts/change_annot_val.py
@@ -12,18 +12,8 @@ for i in annot_list:
     lines = f.readlines()
     sentence_list = ['0' + sentence[1:] for sentence in lines]
     print(sentence_list)
+    f.close()
 
     f2 = open(path + i, 'w')
-    """
-    for line in lines:
-        temp = line.split(' ')
-        # print(len(temp))
-        for t in range(0, len(temp)):
-            if t == 1 and temp[1] == '0':
-                temp[1] = '1'
-                print("temp[1] = 0")
-            elif t == 2 and temp[2] == '0':
-                temp[2] = '1'
-                print("temp[2] = 0")
-        print(temp)
-        """
+    f2.writelines(sentence_list)
+    f2.close()
