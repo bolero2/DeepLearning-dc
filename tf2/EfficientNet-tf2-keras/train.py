c0ee63618d5389fd3cb813dfadd2f0b418f471c6
bolero2 <sheocjf1025@gmail.com>
2021-01-18 17:24:14 +0900

    modified eff/train.py

--- tf2/EfficientNet-tf2-keras/train.py
+++ tf2/EfficientNet-tf2-keras/train.py
@@ -47,6 +47,7 @@ validation_generator = valid_datagen.flow_from_directory(
     target_size=(img_height, img_width),
     batch_size=batch_size,
     class_mode='categorical')
+
 """
 model = Sequential()
 model.add(efn.EfficientNetB3(weights="imagenet", include_top=False, pooling='avg'))
@@ -54,13 +55,13 @@ model.add(layers.Dense(label_size, activation="softmax"))
 model = utils.multi_gpu_model(model, gpus=4)
 model.compile(metrics=['acc'], loss='categorical_crossentropy', optimizer='adam')
 """
+
 mirrored_strategy = tf.distribute.MirroredStrategy()
 
 with mirrored_strategy.scope():
     model = Sequential()
     model.add(efn.EfficientNetB3(weights="imagenet", include_top=False, pooling='avg'))
     model.add(layers.Dense(label_size, activation="softmax"))
-    model = utils.multi_gpu_model(model, gpus=4)
     model.compile(metrics=['acc'], loss='categorical_crossentropy', optimizer='adam')
 # """
 
