2f39a1f8c0ac78401b2b81b4446502e91665db1a
Ubuntu <clt_dc@DCGPU.arkhpeawxyoutnzh1vellybbwc.cx.internal.cloudapp.net>
2021-01-18 03:38:52 +0000

    modified effnet - train.py

--- tf2/EfficientNet-tf2-keras/train.py
+++ tf2/EfficientNet-tf2-keras/train.py
@@ -1,4 +1,5 @@
 import glob
+import os
 from sklearn.model_selection import train_test_split
 import argparse
 from keras.preprocessing.image import ImageDataGenerator
@@ -18,16 +19,17 @@ validation_data_dir = '/home/clt_dc/dataset/classification/cifar-animal/eval'
 nb_train_samples = [len(os.listdir(filelist)) for filelist in [train_data_dir + "/" + num_files + "/" for num_files in sorted(os.listdir(train_data_dir))]]
 nb_validation_samples = [len(os.listdir(filelist)) for filelist in [validation_data_dir + "/" + num_files + "/" for num_files in sorted(os.listdir(validation_data_dir))]]
 n_classes = len(os.listdir(train_data_dir))
-batch_size = 16
+batch_size = 8 
+img_height = img_width = 300
 ###############################################################
 
 train_datagen = ImageDataGenerator(
-    rescale=1. / 255,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True)
+    rescale=1. / 255)
+    # shear_range=0.2,
+    # zoom_range=0.2,
+    # horizontal_flip=True)
 
-test_datagen = ImageDataGenerator(rescale=1. / 255)
+valid_datagen = ImageDataGenerator(rescale=1. / 255)
 
 train_generator = train_datagen.flow_from_directory(
     train_data_dir,
@@ -35,14 +37,14 @@ train_generator = train_datagen.flow_from_directory(
     batch_size=batch_size,
     class_mode='categorical')
 
-validation_generator = test_datagen.flow_from_directory(
+validation_generator = valid_datagen.flow_from_directory(
     validation_data_dir,
     target_size=(img_height, img_width),
     batch_size=batch_size,
     class_mode='categorical')
 
 model = Sequential()
-model.add(efn.EfficientNetB4(weights="imagenet", include_top=False, pooling='avg'))
+model.add(efn.EfficientNetB3(weights="imagenet", include_top=False, pooling='avg'))
 model.add(layers.Dense(n_classes, activation="softmax"))
 model = utils.multi_gpu_model(model, gpus=4)
 model.compile(metrics=['acc'], loss='categorical_crossentropy', optimizer='adam')
