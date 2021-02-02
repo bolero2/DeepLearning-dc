d879d745da036b369dad4950e06643e1160b5378
Ubuntu <clt_dc@DCGPU.arkhpeawxyoutnzh1vellybbwc.cx.internal.cloudapp.net>
2021-02-02 08:21:40 +0000

    clt_dc

--- data-handling-scripts/resize_dataset.py
+++ data-handling-scripts/resize_dataset.py
@@ -3,10 +3,14 @@ import glob
 import cv2
 
 
-path = '/home/clt_dc/dataset/yolo_c16/newendo/'
+path = '/home/clt_dc/dataset/detection/idc_cancer/yolo_c16/new_endo/new_endo_resized/'
+
 os.chdir(path)
-imglist = [x for x in glob.glob('*.jpg')]
+imglist = sorted([x for x in glob.glob('*.jpg')])
 
 for imgname in imglist:
     img = cv2.imread(f'{path}{imgname}')
-    print(img.shape)
+
+    img2 = cv2.resize(img, (640, 480))
+    cv2.imwrite(f'{path}{imgname}', img2)
+    print(img.shape, img2.shape)
