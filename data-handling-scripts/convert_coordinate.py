48b7331c7cf1ab2c543939ba673a07e0ceafc68a
bolero <sheocjf1025@gmail.com>
2021-04-06 13:49:20 +0900

    convert_coordinate 클래스 수정 및 VOC_dataset parsing 스크립트 추가

--- data-handling-scripts/convert_coordinate.py
+++ data-handling-scripts/convert_coordinate.py
@@ -4,252 +4,117 @@ import glob
 import numpy as np
 
 
-def coord2xywh(img_shape, input_coord, input_type, coord):
-    """
-    :param img_shape: row and column
-    :param input_coord: 1. ccwh / 2. xywh / 3. xyrb
-    :param input_type: 1. relat / 2. abs
-    :param coord: coord[0] ~ coord[3]
+class convert_coordinate(object):
+    def __init__(self, coordinate, img_shape, input_coord, input_type, output_coord, output_type):
+        # img_shape = (height, width) 이미지의 세로/가로
+        """
+        Arguments:
+        1) coordinate : bbox coordinate
+        2) img_shape = (height, width)
+        3) input_coord = ['ccwh', 'xyrb', 'xywh']
+        4) input_type = ['relat', 'abs']
+        5) output_coord = ['ccwh', 'xyrb', 'xywh']
+        6) output_type = ['relat', 'abs']
+        return:
+        converted coordinate, self.result
+        example:
+        answer = convert_coordinate(bbox, img_shape=(480, 640), 
+                                    input_coord='ccwh', input_type='relat',
+                                    output_coord='xyrb', output_type='abs')
+        """
 
-    :return: output(shape=[4])
+        self.coord = [float(coordinate[0]), 
+                      float(coordinate[1]), 
+                      float(coordinate[2]), 
+                      float(coordinate[3])]
+        self.ih = img_shape[0]
+        self.iw = img_shape[1]
+        self.input_coord = input_coord
+        self.input_type = input_type
+        self.output_coord = output_coord
+        self.output_type = output_type
 
-    [output standard : xywh + abs]
-     - output[0] = xmin
-     - output[1] = ymin
-     - output[2] = width
-     - output[3] = height
+        self.converted_type = self.convert_type(self.coord, self.input_type, self.output_type)
+        self.converted_coord = self.convert_coord(self.converted_type, self.input_coord, self.output_coord)
+        self.result = self.converted_coord
+        
+    def convert_type(self, coord, input_type, output_type):
+        """
+        coordinate type만 변경.
+        1) relat(상대 좌표) to abs(절대 좌표)
+        2) abs(절대 좌표) to relat(상대 좌표)
+        """
+        result = []
 
-    """
+        if input_type != output_type:
+            if input_type == 'relat' and output_type == 'abs':
+                result = [coord[0] * self.iw, coord[1] * self.ih, coord[2] * self.iw, coord[3] * self.ih]
+            elif input_type == 'abs' and output_type == 'relat':
+                result = [coord[0] / self.iw, coord[1] / self.ih, coord[2] / self.iw, coord[3] / self.ih]
 
-    row = img_shape[0]
-    col = img_shape[1]
-    output = np.zeros(shape=[4])
-    if input_coord == 'ccwh':
-        if input_type == 'relat':
-            output[0] = (coord[0] * col) - (coord[2] * col / 2)
-            output[1] = (coord[1] * row) - (coord[3] * row / 2)
-            output[2] = coord[2] * col
-            output[3] = coord[3] * row
-        elif input_type == 'abs':
-            output[2] = coord[2]
-            output[3] = coord[3]
-            output[0] = coord[0] - (coord[2] / 2)
-            output[1] = coord[1] - (coord[3] / 2)
+        elif input_type == output_type:
+            result = coord
 
-    elif input_coord == 'xywh':
-        if input_type == 'relat':
-            output[0] = coord[0] * col
-            output[1] = coord[1] * row
-            output[2] = coord[2] * col
-            output[3] = coord[3] * row
-        elif input_type == 'abs':
-            output[0] = coord[0]
-            output[1] = coord[1]
-            output[2] = coord[2]
-            output[3] = coord[3]
+        return result
 
-    elif input_coord == 'xyrb':
-        if input_type == 'relat':
-            output[0] = coord[0] * col
-            output[1] = coord[1] * row
-            output[2] = abs(coord[2] - coord[0]) * col
-            output[3] = abs(coord[3] - coord[1]) * row
-        elif input_type == 'abs':
-            output[0] = coord[0]
-            output[1] = coord[1]
-            output[2] = abs(coord[2] - coord[0])
-            output[3] = abs(coord[3] - coord[1])
+    def convert_coord(self, coord, input_coord, output_coord):
+        """
+        coordinate system을 변경.
+        [대상 인자]
+        1) ccwh : center_x, center_y, width, height
+        2) xyrb : xmin, ymin, xmax, ymax
+        3) xywh : xmin, ymin, width, height
+        """
+        result = []
 
-    return output
+        if input_coord != output_coord:
+            if input_coord == 'ccwh':
+                center_x = coord[0]
+                center_y = coord[1]
+                width = coord[2]
+                height = coord[3]
 
-
-def convert_coord_type(input_coord,
-                       input_type,
-                       output_coord,
-                       output_type,
-                       conf_score,
-                       image_path,
-                       label_path,
-                       new_label_path=None):
-    """
-    :param input_coord: 1. ccwh / 2. xywh / 3. xyrb
-    :param input_type: 1. relat / 2. abs
-    :param output_coord: --- Same as input coord. ---
-    :param output_type: --- Same as input type. ---
-
-    :param conf_score: if confidence score information is in annotation text file, then True / or False ---
-
-    :param image_path: Original Color Image path
-    :param label_path: Annotation file path
-    :param new_label_path: Annotation file path to be saved
-
-    :return:Save Annotation text file(.txt) in one-step
-    """
-
-    if new_label_path is None:
-        new_label_path = label_path
-
-    os.chdir(image_path)
-    print("os -> Image path : ", image_path)
-    image_list = list()
-    for file in glob.glob('*.jpg'):
-        image_list.append(file)
-
-    os.chdir(label_path)
-    print("os -> Label path : ", label_path)
-    label_list = list()
-    for file in glob.glob('*.txt'):
-        label_list.append(file)
-
-    full_sentence = list()
-
-    for i in label_list:
-        real_name = i[:-4]
-
-        row, col, ch = cv2.imread(image_path + real_name + ".jpg").shape
-        confidence = 0
-        lines = open(label_path + real_name + ".txt", 'r').readlines()
-
-        for line in lines:
-            temp_sentence = ''
-            line_split = line.split(' ')  # line_split = [class_index, coord[0], coord[1], coord[2], coord[3]]
-            if line_split[-1] == "\n":
-                line_split[-2] = line_split[-2] + line_split[-1]
-                del line_split[-1]  # remove character "\n"
-            print(f"line_split:{line_split}")
-            if conf_score:
-                confidence = float(line_split[1])
-                coord = line_split[2:]
-            else:
-                coord = line_split[1:]
-
-            print(f"coord:{coord}")
-            class_index = int(line_split[0])
-            coord = list(map(float, coord))
-
-            """
-            convert type only
-            1. 'relat' -> 'abs'
-            2.   'abs' -> 'relat'
-            """
-            if input_coord == output_coord:
-                if input_type == 'relat' and output_type == 'abs':
-                    if conf_score:
-                        temp_sentence = str(class_index) + " " + str(confidence) + " " + \
-                                        str(int(coord[0] * col)) + " " + str(int(coord[1] * row)) + " " + \
-                                        str(int(coord[2] * col)) + " " + str(int(coord[3] * row)) + "\n"
-                    else:
-                        temp_sentence = str(class_index) + " " + \
-                                        str(int(coord[0] * col)) + " " + str(int(coord[1] * row)) + " " + \
-                                        str(int(coord[2] * col)) + " " + str(int(coord[3] * row)) + "\n"
-                elif input_type == 'abs' and output_type == 'relat':
-                    if conf_score:
-                        temp_sentence = str(class_index) + " " + str(confidence) + " " + \
-                                        str(coord[0] / col) + " " + str(coord[1] / row) + " " + \
-                                        str(coord[2] / col) + " " + str(coord[3] / row) + "\n"
-                    else:
-                        temp_sentence = str(class_index) + " " + \
-                                        str(coord[0] / col) + " " + str(coord[1] / row) + " " + \
-                                        str(coord[2] / col) + " " + str(coord[3] / row) + "\n"
-                else:
-                    print("What do you want...? There is Error! (\\  '0`/)")
-                    exit(0)
-                full_sentence.append(temp_sentence)
-
-            else:
-                xywh = coord2xywh(img_shape=[row, col], input_coord=input_coord, input_type=input_type, coord=coord)
+                if output_coord == 'xywh':
+                    result = [center_x - (width / 2), center_y - (height / 2), width, height]
+                elif output_coord == 'xyrb':
+                    result = [center_x - (width / 2),
+                              center_y - (height / 2),
+                              center_x + (width / 2),
+                              center_y + (height / 2)]
+            elif input_coord == 'xywh':
+                xmin = coord[0]
+                ymin = coord[1]
+                width = coord[2]
+                height = coord[3]
 
                 if output_coord == 'ccwh':
-                    if output_type == 'relat':
-                        width_relat = xywh[2] / col
-                        height_relat = xywh[3] / row
-                        center_x = float(xywh[0] / col + (width_relat / 2))
-                        center_y = float(xywh[1] / row + (height_relat / 2))
-                        if conf_score:
-                            temp_sentence = str(class_index) + " " + str(confidence) + " " + \
-                                            str(center_x) + " " + str(center_y) + " " + \
-                                            str(width_relat) + " " + str(height_relat) + "\n"
-                        else:
-                            temp_sentence = str(class_index) + " " + \
-                                            str(center_x) + " " + str(center_y) + " " + \
-                                            str(width_relat) + " " + str(height_relat) + "\n"
-                    elif output_type == 'abs':
-                        width = xywh[2]
-                        height = xywh[3]
-                        center_x = int(xywh[0] + (width / 2))
-                        center_y = int(xywh[1] + (height / 2))
-                        if conf_score:
-                            temp_sentence = str(class_index) + " " + str(confidence) + " " + \
-                                            str(center_x) + " " + str(center_y) + " " + \
-                                            str(int(width)) + " " + str(int(height)) + "\n"
-                        else:
-                            temp_sentence = str(class_index) + " " + \
-                                            str(center_x) + " " + str(center_y) + " " + \
-                                            str(int(width)) + " " + str(int(height)) + "\n"
-
-                elif output_coord == 'xywh':
-                    if output_type == 'relat':
-                        if conf_score:
-                            temp_sentence = str(class_index) + " " + str(confidence) + " " + \
-                                            str(xywh[0] / col) + " " + str(xywh[1] / row) + " " + \
-                                            str(xywh[2] / col) + " " + str(xywh[3] / row) + "\n"
-                        else:
-                            temp_sentence = str(class_index) + " " + \
-                                            str(xywh[0] / col) + " " + str(xywh[1] / row) + " " + \
-                                            str(xywh[2] / col) + " " + str(xywh[3] / row) + "\n"
-                    elif output_type == 'abs':
-                        if conf_score:
-                            temp_sentence = str(class_index) + " " + str(confidence) + " " + \
-                                            str(int(xywh[0])) + " " + str(int(xywh[1])) + " " + \
-                                            str(int(xywh[2])) + " " + str(int(xywh[3])) + "\n"
-                        else:
-                            temp_sentence = str(class_index) + " " + \
-                                            str(int(xywh[0])) + " " + str(int(xywh[1])) + " " + \
-                                            str(int(xywh[2])) + " " + str(int(xywh[3])) + "\n"
-
+                    result = [xmin + (width / 2), ymin + (height / 2), width, height]
                 elif output_coord == 'xyrb':
-                    if output_type == 'relat':
-                        if conf_score:
-                            temp_sentence = str(class_index) + " " + str(confidence) + " " + \
-                                            str(float(xywh[0] / col)) + " " + \
-                                            str(float(xywh[1] / row)) + " " + \
-                                            str(float((xywh[0] + xywh[2]) / col)) + " " + \
-                                            str(float((xywh[1] + xywh[3]) / row)) + "\n"
-                        else:
-                            temp_sentence = str(class_index) + " " + \
-                                            str(float(xywh[0] / col)) + " " + \
-                                            str(float(xywh[1] / row)) + " " + \
-                                            str(float((xywh[0] + xywh[2]) / col)) + " " + \
-                                            str(float((xywh[1] + xywh[3]) / row)) + "\n"
-                    elif output_type == 'abs':
-                        if conf_score:
-                            temp_sentence = str(class_index) + " " + str(confidence) + " " + \
-                                            str(int(xywh[0])) + " " + str(int(xywh[1])) + " " + \
-                                            str(int(xywh[0] + xywh[2])) + " " + str(int(xywh[1] + xywh[3])) + "\n"
-                        else:
-                            temp_sentence = str(class_index) + " " + \
-                                            str(int(xywh[0])) + " " + str(int(xywh[1])) + " " + \
-                                            str(int(xywh[0] + xywh[2])) + " " + str(int(xywh[1] + xywh[3])) + "\n"
+                    result = [xmin, ymin, xmin + width, ymin + height]
 
-                full_sentence.append(temp_sentence)
-        print(f"Annot file: {i} -> Convert [{lines}] to [{full_sentence}]")
-        # print(i, full_sentence)
-        new_txt = open(new_label_path + real_name + ".txt", 'w')
-        new_txt.writelines(full_sentence)
-        full_sentence = list()
+            elif input_coord == 'xyrb':
+                xmin = coord[0]
+                ymin = coord[1]
+                xmax = coord[2]
+                ymax = coord[3]
 
+                width = xmax - xmin
+                height = ymax - ymin
+                
+                if output_coord == 'ccwh':
+                    result = [xmin + (width / 2), ymin + (height / 2), width, height]
+                elif output_coord == 'xywh':
+                    result = [xmin, ymin, width, height]
+
+        elif input_coord == output_coord:
+            result = coord
+
+        return result
 
 if __name__ == "__main__":
-    image_path = '/home/bolero/.dc/dl/dataset/detection/idc_cancer/new_endo/new_data/'
-    label_path = '/home/bolero/.dc/dl/dataset/detection/idc_cancer/new_endo/annot_xyrb/'
-    new_label_path = '/home/bolero/.dc/dl/dataset/detection/idc_cancer/new_endo/annot_yolo/'
+    bndbox = [12, 24, 360, 172]
+    ih, iw = (480, 640)
 
-    convert_coord_type(input_coord='xyrb',
-                       input_type='abs',
-                       output_coord='ccwh',
-                       output_type='relat',
-                       conf_score=False,
-                       image_path=image_path,
-                       label_path=label_path,
-                       new_label_path=new_label_path)
+    _bndbox = convert_coordinate(bndbox, (ih, iw), 'xywh', 'abs', 'ccwh', 'relat')
+    bndbox = _bndbox.result
     exit(0)
---
+++ data-handling-scripts/data_parsing/parsing_VOC_dataset.py
@@ -0,0 +1,171 @@
+# -*- coding: utf-8 -*-
+
+import os
+import glob
+import shutil
+import cv2
+import xmltodict
+import json
+
+
+CLASSES = ("aeroplane", "bicycle", "bird", 
+           "boat", "bottle", "bus",
+           "car", "cat", "chair", "cow", 
+           "diningtable", "dog", "horse",
+           "motorbike", "person", "pottedplant", 
+           "sheep", "sofa", "train", "tvmonitor")
+
+
+class convert_coordinate(object):
+    def __init__(self, coordinate, img_shape, input_coord, input_type, output_coord, output_type):
+        # img_shape = (height, width) 이미지의 세로/가로
+        """
+        Arguments:
+        1) coordinate : bbox coordinate
+        2) img_shape = (height, width)
+        3) input_coord = ['ccwh', 'xyrb', 'xywh']
+        4) input_type = ['relat', 'abs']
+        5) output_coord = ['ccwh', 'xyrb', 'xywh']
+        6) output_type = ['relat', 'abs']
+        return:
+        converted coordinate, self.result
+        example:
+        answer = convert_coordinate(bbox, img_shape=(480, 640), 
+                                    input_coord='ccwh', input_type='relat',
+                                    output_coord='xyrb', output_type='abs')
+        """
+
+        self.coord = [float(coordinate[0]), 
+                      float(coordinate[1]), 
+                      float(coordinate[2]), 
+                      float(coordinate[3])]
+        self.ih = img_shape[0]
+        self.iw = img_shape[1]
+        self.input_coord = input_coord
+        self.input_type = input_type
+        self.output_coord = output_coord
+        self.output_type = output_type
+
+        self.converted_type = self.convert_type(self.coord, self.input_type, self.output_type)
+        self.converted_coord = self.convert_coord(self.converted_type, self.input_coord, self.output_coord)
+        self.result = self.converted_coord
+        
+    def convert_type(self, coord, input_type, output_type):
+        """
+        coordinate type만 변경.
+        1) relat(상대 좌표) to abs(절대 좌표)
+        2) abs(절대 좌표) to relat(상대 좌표)
+        """
+        result = []
+
+        if input_type != output_type:
+            if input_type == 'relat' and output_type == 'abs':
+                result = [coord[0] * self.iw, coord[1] * self.ih, coord[2] * self.iw, coord[3] * self.ih]
+            elif input_type == 'abs' and output_type == 'relat':
+                result = [coord[0] / self.iw, coord[1] / self.ih, coord[2] / self.iw, coord[3] / self.ih]
+
+        elif input_type == output_type:
+            result = coord
+
+        return result
+
+    def convert_coord(self, coord, input_coord, output_coord):
+        """
+        coordinate system을 변경.
+        [대상 인자]
+        1) ccwh : center_x, center_y, width, height
+        2) xyrb : xmin, ymin, xmax, ymax
+        3) xywh : xmin, ymin, width, height
+        """
+        result = []
+
+        if input_coord != output_coord:
+            if input_coord == 'ccwh':
+                center_x = coord[0]
+                center_y = coord[1]
+                width = coord[2]
+                height = coord[3]
+
+                if output_coord == 'xywh':
+                    result = [center_x - (width / 2), center_y - (height / 2), width, height]
+                elif output_coord == 'xyrb':
+                    result = [center_x - (width / 2),
+                              center_y - (height / 2),
+                              center_x + (width / 2),
+                              center_y + (height / 2)]
+            elif input_coord == 'xywh':
+                xmin = coord[0]
+                ymin = coord[1]
+                width = coord[2]
+                height = coord[3]
+
+                if output_coord == 'ccwh':
+                    result = [xmin + (width / 2), ymin + (height / 2), width, height]
+                elif output_coord == 'xyrb':
+                    result = [xmin, ymin, xmin + width, ymin + height]
+
+            elif input_coord == 'xyrb':
+                xmin = coord[0]
+                ymin = coord[1]
+                xmax = coord[2]
+                ymax = coord[3]
+
+                width = xmax - xmin
+                height = ymax - ymin
+                
+                if output_coord == 'ccwh':
+                    result = [xmin + (width / 2), ymin + (height / 2), width, height]
+                elif output_coord == 'xywh':
+                    result = [xmin, ymin, width, height]
+
+        elif input_coord == output_coord:
+            result = coord
+
+        return result
+
+
+if __name__ == "__main__":
+    imgpath = "voc2012/JPEGImages/"
+    annotpath = "voc2012/Annotations/"
+    save_path = "voc2012/TextAnnotations/"
+
+    imglist = [x.split('/')[-1] for x in glob.glob(imgpath + '*.jpg')]
+    annotlist = glob.glob(annotpath + "*.xml")
+    print(f"image count: {len(imglist)}")
+    print(f"annotation count: {len(annotlist)}")
+
+    for i in annotlist:
+        file = open(i, 'r').read()
+        xmlfile = xmltodict.parse(file)
+        data = json.loads(json.dumps(xmlfile))
+        
+        filename = data['annotation']['filename']
+        iw = int(data['annotation']['size']['width'])
+        ih = int(data['annotation']['size']['height'])
+        print(f"Filename: {filename} | shape=({ih}, {iw})")
+
+        object = data['annotation']['object']
+        if not isinstance(object, list):
+            object = [object]
+        sentence = []
+        for index in range(0, len(object)):
+            classname = object[index]['name']
+            label = str(CLASSES.index(object[index]['name']))
+            xmin = float(object[index]['bndbox']['xmin'])
+            ymin = float(object[index]['bndbox']['ymin'])
+            xmax = float(object[index]['bndbox']['xmax'])
+            ymax = float(object[index]['bndbox']['ymax'])
+            bbox = [xmin, ymin, xmax, ymax]
+            _bbox = convert_coordinate(bbox, (ih, iw), 'xyrb', 'abs', 'ccwh', 'relat')
+            bbox = _bbox.result
+            sentence.append(f"{label} {round(bbox[0], 5)} {round(bbox[1], 5)} {round(bbox[2], 5)} {round(bbox[3], 5)}\n")
+        print(sentence)
+
+        file = open(save_path + filename[:-3] + "txt", "w")
+        file.writelines(sentence)
+        file.close()
+
+
+
+
+
