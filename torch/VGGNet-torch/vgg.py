408b442765bb48f4312d864f3521f8bc670530e0
bolero2 <sheocjf1025@gmail.com>
2021-05-17 15:17:08 +0900

    vgg.py 수정중

--- torch/VGGNet-torch/vgg.py
+++ torch/VGGNet-torch/vgg.py
@@ -9,21 +9,6 @@ import time
 
 from models import VGGNet
 
-# import neuralworkslib modules for ...... $ python m-512.py
-sys.path.append(os.getenv("NW_HOME") + "/neulib")
-import neuenv
-from job import Job
-
-NW_HOME = neuenv.NW_HOME
-'''
-self.history = torch_model.fit(x=train_data[0], y=train_data[1], 
-                               validation_data=valid_data,
-                               epochs=job.hyper_paras["epochs"], 
-                               batch_size=job.hyper_paras["batch_size"],   # batch_size 추가함
-                               callbacks=[mygetmetric, mysavebestweight])
-ypred = torch_model.predict(test_data[0])
-'''
-
         
 def get_model(input_shape, job=None):
     nw_home = neuenv.NW_HOME
